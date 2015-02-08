533a69f0095bdeca81b56f83eca8332be96d7453
A. Emre Ünal <aemreunal@gmail.com>
2015-02-08 21:44:07 +0200

    Use custom HttpClient to trust self-signed

--- src/main/java/com/aemreunal/iBeaconServerManager.java
+++ src/main/java/com/aemreunal/iBeaconServerManager.java
@@ -16,7 +16,17 @@ package com.aemreunal;
  ***************************
  */
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeUnit;
 import java.util.prefs.Preferences;
+import javax.net.ssl.SSLContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import com.aemreunal.view.ManagerWindow;
 import com.mashape.unirest.http.Unirest;
 
@@ -31,18 +41,47 @@ import com.mashape.unirest.http.Unirest;
  *
  */
 
+/*
+ * Worst case scenario:
+ * http://www.java-samples.com/showtutorial.php?tutorialid=210
+ */
+
 public class iBeaconServerManager {
     private static Preferences preferences;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         new iBeaconServerManager();
     }
 
     public iBeaconServerManager() {
         preferences = Preferences.userRoot().node(this.getClass().getName());
+
+        try {
+            createCustomHttpClient();
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            System.err.println("Unable to create custom HTTP client!");
+            System.err.println("Will exit.");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
         Unirest.setDefaultHeader("Content-Type", "application/json");
         Unirest.setDefaultHeader("Accept", "application/json");
+
         new ManagerWindow();
+    }
+
+    // A custom HTTP client is created in order to
+    private void createCustomHttpClient() throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException {
+        SSLContext sslcontext = SSLContexts.custom()
+                                           .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                                           .build();
+
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext);
+        CloseableHttpClient httpclient = HttpClients.custom()
+                                                    .setSSLSocketFactory(sslsf)
+                                                    .build();
+        Unirest.setHttpClient(httpclient);
     }
 
     public static Preferences getPreferences() {
